5ead02787da5f1df0fda396520fcf0484ad55420
gpfns <70314306+gpfns@users.noreply.github.com>
2021-03-23 08:09:20 +0530

    Update req_controller.py

--- req_controller.py
+++ req_controller.py
@@ -16,4 +16,4 @@ def find_best(temp,hum,ph,rf):
         'Humidity':hum,
                     'pH':ph,
                     'Rainfall':rf})
-app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+app.run(debug=True,host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
