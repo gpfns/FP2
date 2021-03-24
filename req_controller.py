0b87915b21be414ad79996f2959872052dc12463
gpfns <70314306+gpfns@users.noreply.github.com>
2021-03-24 10:45:30 +0530

    Update req_controller.py

--- req_controller.py
+++ req_controller.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, jsonify
+from get_models import grab
 app=Flask(__name__)
 
 @app.route('/bestcrop')
@@ -22,4 +23,38 @@ def find_best(temp,hum,rf,ph=None):
                         'Humidity':hum,
                         'pH':ph,
                         'Rainfall':rf})
+@app.route('/predict/<temp>/<hum>/<rf>/<ph>')
+def predict_best(temp,hum,rf,ph=None):
+    if ph is None:
+        t,h,r=tuple(map(float,[temp,hum,rf]))
+        kpc=pc[1]
+        d1={}
+        k1=1
+        for i in kpc[0]:
+            d1['DT'+k1]=i.predict([t,h,r])
+            k1+=1
+        k1=1    
+        for i in kpc[2]:
+            d1['RFC'+k1]=i.predict([t,h,r])
+            k1+=1
+        d1['NBC']=kpc[1].predict([t,h,r])    
+            
+        return jsonify(d1)
+    else:    
+        t,h,p,r = tuple(map(float,[temp,hum,ph,rf]))
+        kpc=pc[0]
+        d1={}
+        k1=1
+        for i in kpc[0]:
+            d1['DT'+k1]=i.predict([t,h,p,r])
+            k1+=1
+        k1=1    
+        for i in kpc[2]:
+            d1['RFC'+k1]=i.predict([t,h,p,r])
+            k1+=1
+        d1['NBC']=kpc[1].predict([t,h,p,r])    
+            
+        return jsonify(d1)
+        
+pc = grab()    
 app.run(debug=True,host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
