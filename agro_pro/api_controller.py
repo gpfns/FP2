97e1f6208352c1fa3e131fd6e96461d0a61f54c3
gpfns <70314306+gpfns@users.noreply.github.com>
2021-05-23 01:43:52 +0530

    API Fly

--- agro_pro/api_controller.py
+++ agro_pro/api_controller.py
@@ -1,20 +1,24 @@
 from agro_pro import app
 from flask import jsonify, render_template, request, redirect, url_for
-from agro_pro.api_tools import *
+from agro_pro.api_tools import api_test_tool, api_get_climate_by_lat_long, api_get_forecast_by_lat_long
 
 
 @app.route('/api_test')
-def fun_api_test():
+def cont_fun_api_test():
     a1 = request.args['lat']
     a2 = request.args['lon']
     return api_test_tool(a1, a2)
 
 
-@app.route('/api_weather')
-def fun_api_weather_forecast():
-    pass
+@app.route('/api_weather_forecast')
+def cont_fun_api_weather_forecast():
+    a1 = request.args['lat']
+    a2 = request.args['lon']
+    return api_get_forecast_by_lat_long(a1, a2)
 
 
-@app.route('/api_forecast')
-def fun_api_climate_details():
-    pass
+@app.route('/api_climate')
+def cont_fun_api_climate_details():
+    a1 = request.args['lat']
+    a2 = request.args['lon']
+    return api_get_climate_by_lat_long(a1, a2)
--- agro_pro/api_tools.py
+++ agro_pro/api_tools.py
@@ -1,3 +1,6 @@
+import requests
+
+
 def api_test_tool(lat, lon):
     return """<html>
     <body>
@@ -8,3 +11,116 @@ def api_test_tool(lat, lon):
     <b> Amma baboi </b>
     <i> Italics mari </i>
     </body>""".format(lat, lon)
+
+
+def api_get_forecast_by_lat_long(latitude, longitude, no_of_days=7):
+    parameters = {
+        'key': '6a95b306f1334a3a87c190139212005',
+        'q': str(latitude) + ',' + str(longitude),
+        'aqi': 'no',
+        'alerts': 'no',
+        'days': no_of_days
+    }
+    url1 = 'http://api.weatherapi.com/v1/forecast.json'
+    response = requests.get(url1, params=parameters)
+    data = response.json()
+    output = api_fun_parse_future_forecast(data)
+    return output
+
+
+def api_get_climate_by_lat_long(latitude, longitude):
+    parameters = {
+        'key': '6a95b306f1334a3a87c190139212005',
+        'q': str(latitude) + ',' + str(longitude),
+        'aqi': 'no'
+    }
+    url1 = 'http://api.weatherapi.com/v1/current.json'
+    response = requests.get(url1, params=parameters)
+    data = response.json()
+    output = api_fun_parse_climate(data)
+    return output
+
+
+def api_fun_parse_climate(data):
+    output = ''
+    location = data['location']
+    output += b_parse_location(location)
+    current = data['current']
+    output += b_parse_current(current)
+
+    return output
+
+
+def api_fun_parse_future_forecast(data):
+    output = ''
+    location = data['location']
+    output += b_parse_location(location)
+    output += '<br>'
+
+    current = data['current']
+    output += b_parse_current(current)
+    output += '<br>'
+
+    forecast_data = data['forecast']['forecastday']
+    output += b_parse_forecast_days(forecast_data)
+
+    # forecast_alerts = data['forecast']['alerts']
+
+    return output
+
+
+def b_parse_location(data):
+    s1 = "<h2> Location Details </h2><p>"
+    s1 += "Selected Place : " + data['name'] + '<br>'
+    s1 += "Region : " + data['region'] + '<br>'
+    s1 += "Country : " + data['country'] + '<br>'
+    s1 += "Latitude : " + str(data['lat']) + '<br>'
+    s1 += "Longitude : " + str(data['lon']) + '<br>'
+    s1 += "Local Time : " + data['localtime'] + '<br>'
+    s1 += '</p>'
+
+    return s1
+
+
+def b_parse_current(data):
+    s1 = "<h2> Current Climate Details </h2><p>"
+    s1 += "Temperature : " + str(data['temp_c']) + '<br>'
+    s1 += "Humidity : " + str(data['humidity']) + '<br>'
+    s1 += "Condition : " + data['condition']['text'] + '<br>'
+    s1 += "Wind (KPH) : " + str(data['wind_kph']) + '<br>'
+    s1 += "Wind Direction : " + data['wind_dir'] + '<br>'
+    s1 += "Cloud : " + str(data['cloud']) + '<br>'
+    s1 += "Last Updated on : " + data['last_updated'] + '<br>'
+    s1 += '</p>'
+
+    return s1
+
+
+def b_parse_forecast_days(data):
+    """Input forecastday in forecast"""
+    # l = []
+    s1 = "<h2> Weather Forecast Data </h2><p>"
+    # s2 = ''
+    for i in data:
+        s1 += 'Date : ' + i['date'] + '<br>'
+        s1 += 'Details : ' + '<br>'
+        s1 += 'Maximum Temperature -' + str(i['day']['maxtemp_c']) + '<br>'
+        s1 += 'Minimum Temperature -' + str(i['day']['mintemp_c']) + '<br>'
+        s1 += 'Average Temperature -' + str(i['day']['avgtemp_c']) + '<br>'
+        s1 += 'Condition -' + i['day']['condition']['text'] + '<br>'
+        s1 += '<br>'
+        s1 += 'Hourly Climate Details<br><br>'
+        for j in i['hour']:
+            s1 += 'Time - ' + j['time'] + '<br>'
+            s1 += 'Temperature - ' + str(j['temp_c']) + '<br>'
+            s1 += 'Condition - ' + j['condition']['text'] + '<br>'
+            s1 += 'Wind (KPH) - ' + str(j['wind_kph']) + '<br>'
+            s1 += 'Humidity - ' + str(j['humidity']) + '<br>'
+            s1 += 'Cloud - ' + str(j['cloud']) + '<br>'
+            s1 += 'Chance of Rain (%) - ' + j['chance_of_rain'] + '<br>'
+            s1 += '<br>'
+        s1 += '<br>'
+
+    s1 += '</p>'
+
+    return s1
